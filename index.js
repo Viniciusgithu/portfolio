ec050a347cb7204bdfaa80c7b017c9514cd3b14a
Vinicius Lima <viniciuslimavirtuel@gmail.com>
2022-12-28 15:43:14 -0300

    fourth commit

--- index.js
+++ index.js
@@ -1,6 +1,24 @@
 let attention = document.querySelector('.attention');
 let test = 0;
 
+const title = document.querySelector('h1');
+const developer = document.querySelector('#dev');
+
+function typeWriter(element){
+
+  const textArray = element.innerHTML.split('');
+  element.innerHTML = '';
+  textArray.forEach((lyrics, i) => {
+
+    setTimeout(() => element.innerHTML += lyrics, 150 * i);
+
+  });
+
+}
+
+
+
+
 function blink (){
 
   if (test < 1){
@@ -13,5 +31,8 @@ function blink (){
 
 }
 
+
+
+typeWriter(title)
 
 setInterval(blink, 500)
